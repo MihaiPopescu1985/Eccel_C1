c3632568315a056510a3b63133d8fd4fb06f399b
Mihai <justmaryo@gmail.com>
2021-01-01 14:07:17 +0200

    parsing card uid and device name after a card scan

--- c1device/test/websocket_test.go
+++ c1device/test/websocket_test.go
@@ -41,6 +41,18 @@ func TestReceivingMessageViaWebsocket(t *testing.T) {
 }
 
 // go test -run ReceivingAllMessagesViaWebsocket
+/*
+Example of card reading
+{
+        "type": "uid",
+        "uid":  "045D91B22C5E80",
+        "sak":  0,
+        "string":       "MIFARE Ultralight",
+        "device-name":  "Pepper_C1-1A6318",
+        "memory":       "045D9140B22C5E8040480000E110120005060708090A0B0C0000000000000000",
+        "known_tag":    true
+}
+*/
 func TestReceivingAllMessagesViaWebsocket(t *testing.T) {
 
 	device := c1device.C1Device{
@@ -61,4 +73,42 @@ func TestReceivingAllMessagesViaWebsocket(t *testing.T) {
 	}()
 
 	time.Sleep(time.Second * 10)
+}
+
+func TestParseMessageMethod(t *testing.T) {
+
+	jsonMessage := []byte{123, 10, 9, 34, 116, 121, 112, 101, 34,
+		58, 9, 34, 117, 105, 100, 34, 44, 10, 9, 34, 117, 105,
+		100, 34, 58, 9, 34, 48, 52, 53, 68, 57, 49, 66, 50,
+		50, 67, 53, 69, 56, 48, 34, 44, 10, 9, 34, 115, 97,
+		107, 34, 58, 9, 48, 44, 10, 9, 34, 115, 116, 114, 105,
+		110, 103, 34, 58, 9, 34, 77, 73, 70, 65, 82, 69, 32, 85,
+		108, 116, 114, 97, 108, 105, 103, 104, 116, 34, 44, 10,
+		9, 34, 100, 101, 118, 105, 99, 101, 45, 110, 97, 109, 101,
+		34, 58, 9, 34, 80, 101, 112, 112, 101, 114, 95, 67, 49,
+		45, 49, 65, 54, 51, 49, 56, 34, 44, 10, 9, 34, 109, 101,
+		109, 111, 114, 121, 34, 58, 9, 34, 48, 52, 53, 68, 57, 49,
+		52, 48, 66, 50, 50, 67, 53, 69, 56, 48, 52, 48, 52, 56, 48,
+		48, 48, 48, 69, 49, 49, 48, 49, 50, 48, 48, 48, 53, 48, 54,
+		48, 55, 48, 56, 48, 57, 48, 65, 48, 66, 48, 67, 48, 48, 48,
+		48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 34, 44,
+		10, 9, 34, 107, 110, 111, 119, 110, 95, 116, 97, 103, 34, 58,
+		9, 116, 114, 117, 101, 10, 125}
+
+	expectedDeviceName := "Pepper_C1-1A6318"
+	expectedCardUID := "045D91B22C5E80"
+
+	device := c1device.C1Device{}
+
+	gotDeviceMessage := device.ParseMessage(jsonMessage)
+
+	if expectedDeviceName != gotDeviceMessage.DeviceName {
+		t.Error("got device name = " + gotDeviceMessage.DeviceName)
+		t.Error("got device name != expected device name")
+		t.FailNow()
+	}
+	if expectedCardUID != gotDeviceMessage.CardUID {
+		t.Error("Got card UID != expected card UID")
+		t.FailNow()
+	}
 }
