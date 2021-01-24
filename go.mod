e766fcbdccd1a445af637bc9d048a449381d2c5a
Mihai <justmaryo@gmail.com>
2021-01-24 10:00:15 +0200

    running homepage

--- go.mod
+++ go.mod
@@ -4,7 +4,8 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/gorilla/websocket v1.4.2
-	golang.org/x/net v0.0.0-20201110031124-69a78807bb2b
-	golang.org/x/text v0.3.4 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/net v0.0.0-20210119194325-5f4716e94777
+	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,113 +1,16 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
-	"example.com/c1/c1device"
-	"example.com/c1/service"
+	"example.com/c1/web/controller"
 )
 
 func main() {
-	fmt.Println("Hello world!")
 
-	var dao service.DAO
-	dao.Connect()
-
-	if !dao.IsConnected() {
-		fmt.Println("No database connection")
+	handler := http.HandlerFunc(controller.HomePageHandler)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatalf("could not listen on port 8080 %v", err)
 	}
-
-	device1 := c1device.C1Device{
-		IP:        "192.168.0.10",
-		WsChannel: make(chan []byte),
-	}
-	device1.WsConnect()
-	device1.WsRead()
-
-	go func() {
-		for msg := range device1.WsChannel {
-
-			deviceName, cardUID := device1.ParseMessage(msg)
-
-			command :=
-				dao.InsertIntoWorkday(deviceName, cardUID)
-
-			if dao.IsConnected() && deviceName != "" && cardUID != "" {
-				fmt.Println(command)
-				dao.Execute(command)
-			}
-			device1.WsRead()
-		}
-	}()
-
-	device2 := c1device.C1Device{
-		IP:        "192.168.0.20",
-		WsChannel: make(chan []byte),
-	}
-	device2.WsConnect()
-	device2.WsRead()
-
-	go func() {
-		for msg := range device2.WsChannel {
-
-			deviceName, cardUID := device2.ParseMessage(msg)
-
-			command :=
-				dao.InsertIntoWorkday(deviceName, cardUID)
-
-			if dao.IsConnected() && deviceName != "" && cardUID != "" {
-				fmt.Println(command)
-				dao.Execute(command)
-			}
-			device2.WsRead()
-		}
-	}()
-
-	device3 := c1device.C1Device{
-		IP:        "192.168.0.30",
-		WsChannel: make(chan []byte),
-	}
-	device3.WsConnect()
-	device3.WsRead()
-
-	go func() {
-		for msg := range device3.WsChannel {
-
-			deviceName, cardUID := device3.ParseMessage(msg)
-
-			command :=
-				dao.InsertIntoWorkday(deviceName, cardUID)
-
-			if dao.IsConnected() && deviceName != "" && cardUID != "" {
-				fmt.Println(command)
-				dao.Execute(command)
-			}
-			device3.WsRead()
-		}
-	}()
-
-	device4 := c1device.C1Device{
-		IP:        "192.168.0.40",
-		WsChannel: make(chan []byte),
-	}
-	device4.WsConnect()
-	device4.WsRead()
-
-	go func() {
-		for msg := range device4.WsChannel {
-
-			deviceName, cardUID := device4.ParseMessage(msg)
-
-			command :=
-				dao.InsertIntoWorkday(deviceName, cardUID)
-
-			if dao.IsConnected() && deviceName != "" && cardUID != "" {
-				fmt.Println(command)
-				dao.Execute(command)
-			}
-			device4.WsRead()
-		}
-	}()
-
-	fmt.Scanln()
 }
